a352d2c8959f34ecaa6b3fdbddd80df7f0c66246
Conrad <sponrad@gmail.com>
2016-01-26 12:29:41 -0800

    changes to android import files

--- playstoreimport.py
+++ playstoreimport.py
@@ -9,6 +9,10 @@ def generateName(row):
         ]
     return " ".join(temp)
 
+def getPrice(row):
+    return str( int( float(row["price"])*1000000 ) )
+
+
 crdict = csv.DictReader(open("ffinput.csv","rb"))
 print("Opening file...")
 f = open('androidimport.csv', 'w')
@@ -21,20 +25,18 @@ print("Processing data...")
 
 data = []
 for row in enumerate(crdict, start=1):
-    sku = "smarterappsflashforce"
-    productid = row[1]["productid"].strip()  #no trailing or leading spaces
-    referencename = generateName(row[1])
-    itemtype = "Non-Consumable"
-    description = "Flash Force pattern"
-    screenshotpath = "/Users/conradframe/Desktop/ffimages/purchaseExample.png"
-    effectivedate = "Now"
-    enddate = "None"
-    notes = "Light pattern and timing combination for use in the Flash Force app"
-    hostedpath = ""
+    product_id = row[1]["productid"].strip()  #no trailing or leading spaces
+    publish_state = "published"
+    purchase_type = "managed_by_android"
+    autotranslate = "false"
+    localetitledescription = "en_US; "+generateName(row[1]) + "; Flash Force Pattern"
+    autofill = "true"
+    countryprice = "US; "+getPrice(row[1])
 
-    itemorder = [sku, productid, referencename, itemtype, "yes", 1, referencename, description, screenshotpath, effectivedate, enddate, notes, hostedpath]
+    #itemorder = [sku, productid, referencename, itemtype, "yes", 1, referencename, description, screenshotpath, effectivedate, enddate, notes, hostedpath]
+    itemorder = [product_id, publish_state, purchase_type, autotranslate, localetitledescription, autofill, countryprice]
     
-    data.append(",".join([str(i) for i in itemorder]))
+    data.append(itemorder)
 
 a.writerows(data)
 f.close()
