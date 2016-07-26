1b9ed1d4a8f49a92ef974eae90496584a395ecdf
sponrad <sponrad@gmail.com>
2016-07-25 21:17:46 -0700

    ios IAP changes

--- appstoreimport.py
+++ appstoreimport.py
@@ -15,6 +15,14 @@ crdict = csv.DictReader(open("ffinput.csv","rb"))
 print("Opening file...")
 f = open('appstoreimport.txt', 'w')
 
+original = csv.DictReader(open("ffinput first approved.csv","rb"))
+originalproductids = []
+
+for row in enumerate(original, start=1):
+    originalproductids.append(row[1]["productid"].strip())
+
+print originalproductids
+
 f.write("""SKU	Product ID	Reference Name	Type	Cleared For Sale	Wholesale Price Tier	Displayed Name	Description	Screenshot Path	Effective Date	End Date	Review Notes	Hosted Content Path
 """)
 
@@ -22,6 +30,9 @@ print("Processing data...")
 
 data = ""
 for row in enumerate(crdict, start=1):
+    if row[1]["productid"].strip() in originalproductids:
+        print(row[1]["productid"].strip())
+        continue
     sku = "smarterappsflashforce"
     productid = row[1]["productid"].strip()  #no trailing or leading spaces
     referencename = generateName(row[1])
